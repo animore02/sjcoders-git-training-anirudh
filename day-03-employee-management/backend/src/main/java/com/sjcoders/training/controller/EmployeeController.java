4f869e54cef26161870efc521af4b35f09411e1e
Anirudh More <animore0856@gmail.com>
2026-08-27 16:11:00 +0530

    feat: implement employee rest api

---
+++ day-03-employee-management/backend/src/main/java/com/sjcoders/training/controller/EmployeeController.java
@@ -0,0 +1,48 @@
+package com.sjcoders.training.controller;
+
+import com.sjcoders.training.model.Employee;
+import com.sjcoders.training.service.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/employees")
+@CrossOrigin(origins = "*")
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Employee> createEmployee(@RequestBody Employee employee) {
+        Employee savedEmployee = employeeService.createEmployee(employee);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedEmployee);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Employee>> getAllEmployees() {
+        return ResponseEntity.ok(employeeService.getAllEmployees());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+
+        return employeeService.getEmployeeById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<Employee>> searchEmployees(
+            @RequestParam String query) {
+
+        return ResponseEntity.ok(employeeService.searchEmployees(query));
+    }
+}
---
+++ day-03-employee-management/backend/src/main/java/com/sjcoders/training/service/EmployeeService.java
@@ -0,0 +1,36 @@
+package com.sjcoders.training.service;
+
+import com.sjcoders.training.model.Employee;
+import com.sjcoders.training.repository.EmployeeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeService{
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public Employee createEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public Optional<Employee> getEmployeeById(Long id) {
+        return employeeRepository.findById(id);
+    }
+
+    public List<Employee> searchEmployees(String query) {
+        return employeeRepository
+                .findByFullNameContainingIgnoreCaseOrEmployeeCodeContainingIgnoreCase(
+                        query, query);
+    }
+}
