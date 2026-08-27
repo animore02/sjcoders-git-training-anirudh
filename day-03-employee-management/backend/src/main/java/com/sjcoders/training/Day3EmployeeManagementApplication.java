20e972818948b1858183821e0d928570f23edf18
Anirudh More <animore0856@gmail.com>
2026-08-27 15:21:42 +0530

    feat: configure mysql database connection

---
+++ day-03-employee-management/backend/src/main/java/com/sjcoders/training/Day3EmployeeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.sjcoders.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day3EmployeeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day3EmployeeManagementApplication.class, args);
+	}
+
+}
