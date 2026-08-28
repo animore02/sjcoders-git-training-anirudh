8a042264841ce48630666ef7a6ceaffc8da00753
Anirudh More <animore0856@gmail.com>
2026-08-28 12:26:57 +0530

    chore: setup React frontend with Bootstrap

---
+++ day-04-react-frontend/src/App.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div >
+      <h1 style={{ textAlign: "center" }}>Employee Management System </h1>
+
+      <p style={{ textAlign: "center" }} > Day 4 - React Frontend </p>
+    </div>
+  );
+}
+
